3d87ed1020fd6b92bcaca1b1a123a313ee1d2b72
antipovaya <antipovayana01@gmail.com>
2025-07-18 10:50:54 +0300

    error search

--- less_1/task01_binary_search.py
+++ less_1/task01_binary_search.py
@@ -49,6 +49,8 @@ def find_number(lst, number):
 
 number_lst = [[1, 3, 4, 2, 5, 7, 6, 8, 9], [1, 3, 4, 2, 5, 7, 6, 8, 9], [
     1, 3, 4, 2, 5, 7, 6, 8, 9], [1, 3, 4, 2, 7, 6, 8, 9]]
-searched_number = 1
+searched_number = 7
 
 print(find_number(number_lst, searched_number))
+
+
